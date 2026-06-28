018720680d37d7841260553cc2de60cde14f8d84
Vedran Hrabar <vedran.hrabar@outlook.com>
2026-06-28 13:03:07 +0200

    feat(cuda): add declarations and shared memory configuration for CUDA kernels
    Signed-off-by: Vedran Hrabar <vedran.hrabar@outlook.com>

--- packages/morphottention/csrc/cuda/utils/declarations.cuh
+++ packages/morphottention/csrc/cuda/utils/declarations.cuh
@@ -1,7 +1,6 @@
 #ifndef MORPHOTTENTION_DECLARATIONS_CUH
 #define MORPHOTTENTION_DECLARATIONS_CUH
 
-
 // ---------------------------------------------------------------------------
 // Threadblock geometry
 // ---------------------------------------------------------------------------
@@ -10,7 +9,7 @@
 #endif
 
 constexpr int BLOCK_SIZE = KERNEL_BLOCK_SIZE;
-constexpr int WARPS      = BLOCK_SIZE / 32;
+constexpr int WARPS = BLOCK_SIZE / 32;
 
 // ---------------------------------------------------------------------------
 // Sequence tiles (forward)
@@ -37,7 +36,7 @@ constexpr int BC_FWD = KERNEL_BC_FWD;
 #define KERNEL_HEAD_DIM_V 64
 #endif
 
-constexpr int CUBE_M_FWD     = KERNEL_CUBE_M;
+constexpr int CUBE_M_FWD = KERNEL_CUBE_M;
 constexpr int HEAD_DIM_V_FWD = KERNEL_HEAD_DIM_V;
 
 constexpr int TILE_M_FWD = BR_FWD;
@@ -67,7 +66,6 @@ constexpr int TILE_N_BWD = BC_BWD;
 #endif
 constexpr bool CAUSAL = (MORPHO_CAUSAL != 0);
 
-
 #ifndef MORPHO_MASK_DIAG
 #define MORPHO_MASK_DIAG 0
 #endif
@@ -78,7 +76,6 @@ static_assert(BC_FWD % 16 == 0, "BC_FWD must be %16 (it is K of GEMM-2)");
 static_assert(CUBE_M_FWD % 16 == 0, "CUBE_M must be %16 (K of GEMM-1)");
 static_assert(HEAD_DIM_V_FWD % 8 == 0, "HEAD_DIM_V must be %8 (N of GEMM-2)");
 static_assert(BR_BWD % 16 == 0 && BC_BWD % 16 == 0, "bwd tiles must align to MMA atom");
-static_assert((BR_FWD * BC_FWD) % (WARPS * 16 * 8) == 0,"score tile does not partition evenly across warps");
-
+static_assert((BR_FWD * BC_FWD) % (WARPS * 16 * 8) == 0, "score tile does not partition evenly across warps");
 
 #endif // MORPHOTTENTION_DECLARATIONS_CUH
--- packages/morphottention/csrc/cuda/utils/smem.cuh
+++ packages/morphottention/csrc/cuda/utils/smem.cuh
@@ -1,11 +1,9 @@
 #ifndef MORPHOTTENTION_SMEM_CUH
 #define MORPHOTTENTION_SMEM_CUH
 
-
 #include <ATen/ATen.h>
 #include <ATen/cuda/CUDAContext.h>
 #include <c10/cuda/CUDAException.h>
-
 
 // set SMEM size (https://leimao.github.io/blog/CUDA-Shared-Memory-Capacity)
 template <typename KernelFn>
@@ -16,8 +14,12 @@ void configure_kernel_smem(KernelFn kernel, const size_t requested, int& cached,
         C10_CUDA_CHECK(cudaGetDevice(&dev));
         C10_CUDA_CHECK(cudaDeviceGetAttribute(&device_limit, cudaDevAttrMaxSharedMemoryPerBlockOptin, dev));
     }
-    TORCH_CHECK(static_cast<int>(requested) <= device_limit, name, " requires ", requested,
-                " bytes of dynamic smem, exceeds device opt-in limit ", device_limit);
+    TORCH_CHECK(static_cast<int>(requested) <= device_limit,
+                name,
+                " requires ",
+                requested,
+                " bytes of dynamic smem, exceeds device opt-in limit ",
+                device_limit);
     if (static_cast<int>(requested) != cached) {
         C10_CUDA_CHECK(
             cudaFuncSetAttribute(kernel, cudaFuncAttributeMaxDynamicSharedMemorySize, static_cast<int>(requested)));
