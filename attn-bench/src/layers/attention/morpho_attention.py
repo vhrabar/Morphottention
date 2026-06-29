f3bc6c46d1f29cc07f26591ee7aadc57997b45e6
Vedran Hrabar <vedran.hrabar@outlook.com>
2026-06-29 12:58:41 +0200

    feat(attention): register FWD in bench #13
    Signed-off-by: Vedran Hrabar <vedran.hrabar@outlook.com>

--- attn-bench/src/layers/attention/morpho_attention.py
+++ attn-bench/src/layers/attention/morpho_attention.py
@@ -1,7 +1,9 @@
-from typing import cast
+from typing import Any, cast
 
 import torch
 from torch import nn
+
+from morphottention import MorphoAttention
 
 
 class MultiHeadSelfAttention(nn.Module):
@@ -9,31 +11,31 @@ class MultiHeadSelfAttention(nn.Module):
     wrapper for custom Multi-Head Self-Attention implemntations
     """
 
-    def __init__(self, dim: int, num_heads: int, dropout: float = 0.0) -> None:
+    HEAD_DIM = 64
+    CUBE_M = 64
+
+    # fmt: off
+    def __init__(self, dim: int, num_heads: int, dropout: float = 0.0,*, cube_m: int = CUBE_M,
+                 scale: float | None = None, causal: bool = False, **_: Any) -> None:
+        # fmt: on
         super().__init__()
-        self.name = "attention"
+        self.name = "morpho_attention"
         self.num_heads = num_heads
-
         self.head_dim = dim // num_heads
-        self.scale = self.head_dim**-0.5
 
+        self.cube_m = cube_m
+
+        self.scale = scale if scale is not None else cube_m**-0.5
+        self.causal = causal
+
+        self.attn = MorphoAttention(dim, num_heads, cube_m, scale=self.scale, causal=causal)
         self.proj = nn.Linear(dim, dim)
         self.dropout = nn.Dropout(dropout)
 
-    def _project(self) -> tuple[torch.Tensor]:
-        raise NotImplementedError("CUDA extension not implemented yet")
-
-    def _attention(self) -> torch.Tensor:
-        raise NotImplementedError("CUDA extension not implemented yet")
-
-    def _invert_out(self, out: torch.Tensor) -> torch.Tensor:
-        BH, N, D = out.shape
-        B = BH // self.num_heads
-        C = self.num_heads * D
-        out = out.view(B, self.num_heads, N, D)
-        out = out.transpose(1, 2).reshape(B, N, C)
+    def forward(
+        self, x: torch.Tensor, k: torch.Tensor, v: torch.Tensor, attn_bias: torch.Tensor | None = None
+    ) -> tuple[torch.Tensor, None]:
+        orig_dtype = x.dtype
+        out = self.attn(x.to(torch.float16)).to(orig_dtype)
         out = self.proj(out)
-        return cast(torch.Tensor, self.dropout(out))
-
-    def forward(self) -> tuple[torch.Tensor, None]:
-        raise NotImplementedError("CUDA extension not implemented yet")
+        return cast(torch.Tensor, self.dropout(out)), None
