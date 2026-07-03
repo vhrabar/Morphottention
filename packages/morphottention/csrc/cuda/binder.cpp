c00ade66ae4cd4010d74be11a98d104b194aaa12
Vedran Hrabar <vedran.hrabar@outlook.com>
2026-07-03 15:16:36 +0200

    feat(binder): update morpho attention registration for HF and add extension registration #21
    Signed-off-by: Vedran Hrabar <vedran.hrabar@outlook.com>

--- packages/morphottention/csrc/cuda/binder.cpp
+++ packages/morphottention/csrc/cuda/binder.cpp
@@ -1,16 +1,17 @@
 #include "dispatch.h"
+#include "registration.h"
 
 #include <torch/library.h>
 
-TORCH_LIBRARY(morphottention, m) {
+TORCH_LIBRARY_EXPAND(TORCH_EXTENSION_NAME, m) {
     m.def("forward(Tensor X, Tensor W_phi, Tensor gate_q, Tensor gate_k, Tensor W_V, "
           "int H, int cube_m, float scale, bool causal) -> Tensor[]");
 
     m.def("backward(Tensor grad_out, Tensor X, Tensor W_phi, Tensor gate_q, Tensor gate_k, "
           "Tensor W_V, Tensor out, Tensor lse, int H, int cube_m, float scale, bool causal) -> Tensor[]");
+
+    m.impl("forward", torch::kCUDA, &forward);
+    m.impl("backward", torch::kCUDA, &backward);
 }
 
-TORCH_LIBRARY_IMPL(morphottention, CUDA, m) {
-    m.impl("forward", &forward);
-    m.impl("backward", &backward);
-}
+REGISTER_EXTENSION(TORCH_EXTENSION_NAME)
